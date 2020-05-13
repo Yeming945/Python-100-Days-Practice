2b42061924e7df1667f80288c158221ebee05f74
Yeming945 <yeming945@qq.com>
2020-05-13 20:00:39 +0800

    17day算法

--- Day16-20/17-1.py
+++ Day16-20/17-1.py
@@ -42,8 +42,82 @@ def bubble_sort(items, comp=lambda x, y: x > y):
     return items
 
 
+def bubble_sort_new(items, comp=lambda x, y: x > y):
+    """ 搅拌排序(冒泡排序升级版) """
+    items = items[:]
+    for i in range(len(items) - 1):
+        swapped = False
+        for j in range(i, len(items) - 1 - i):
+            if comp(items[j], items[j + 1]):
+                items[j], items[j + 1] = items[j + 1], items[j]
+                swapped = True
+        if swapped:
+            swapped = False
+            for j in range(len(items) - 2 - i, i, -1):
+                if comp(items[j - 1], items[j]):
+                    items[j], items[j - 1] = items[j - 1], items[j]
+                    swapped = True
+        if not swapped:
+            break
+    return items
+
+
+def merge(items, items, comp=lambda x, y: x < y):
+    """ 合并(将两个有序的列表合并成一个有序的列表) """
+    items = []
+    index1, index2 = 0, 0
+    while index1 < len(items1) and index2 < len(items2):
+        if comp(items1[index1], items2[index2]):
+            items.append(items1[index1])
+            index1 += 1
+        else:
+            items.append(items2[index2])
+            index2 += 1
+    items += items1[index1:]
+    items += items2[index2:]
+    return items
+
+
+def merge_sort(items, comp=lambda x, y: x < y):
+    return _merge_sort(list(items), comp)
+
+
+def _merge_sort(items, comp):
+    """ 归并排序 """
+    if len(items) < 2:
+        return items
+    mid = len(items) // 2
+    left = _merge_sort(items[:mid], comp)
+    right = _merge_sort(items[mid:], comp)
+    return merge(left, right, comp)
+
+
+def seq_search(items, key):
+    """ 顺序查找 """
+    for index, item in enumerate(items):
+        if item == key:
+            return index
+    return -1
+
+
+def bin_search(items, key):
+    """ 折半查找 """
+    start, end = 0, len(items) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if key > items[mid]:
+            start = mid + 1
+        elif key < items[mid]:
+            end = mid - 1
+        else:
+            return mid
+    return -1
+
+
 if __name__ == "__main__":
     ss = select_sort([24, 25, 23, 55, 32, 44])
     bs = bubble_sort([24, 25, 23, 55, 32, 44])
+    bsn = bubble_sort_new([24, 25, 23, 55, 32, 44])
     print(ss)
     print(bs)
+    print(bsn)
---
+++ Day16-20/17-2.py
@@ -0,0 +1,186 @@
+
+import sys
+import time
+
+
+def case1():
+    """ 穷举法1 百钱百鸡"""
+    # 公鸡5元一只 母鸡3元一只 小鸡1元三只
+    # 用100元买100只鸡 问公鸡/母鸡/小鸡各多少只
+    for x in range(20):
+        for y in range(33):
+            z = 100 - x - y
+            if 5 * x + 3 * y + z // 3 == 100 and z % 3 == 0:
+                print(x, y, z)
+
+
+def case2():
+    """ 穷举法2 五人分鱼 """
+    # A、B、C、D、E五人在某天夜里合伙捕鱼 最后疲惫不堪各自睡觉
+    # 第二天A第一个醒来 他将鱼分为5份 扔掉多余的1条 拿走自己的一份
+    # B第二个醒来 也将鱼分为5份 扔掉多余的1条 拿走自己的一份
+    # 然后C、D、E依次醒来也按同样的方式分鱼 问他们至少捕了多少条鱼
+    fish = 6
+    while True:
+        total = fish
+        enough = True
+        for _ in range(5):
+            if (total - 1) % 5 == 0:
+                total = (total - 1) // 5 * 4
+            else:
+                enough = False
+                break
+        if enough:
+            print(fish)
+            break
+        fish += 5
+
+
+class Thing(object):
+    """
+    贪婪法：在对问题求解时，总是做出在当前看来是最好的选择，不追求最优解，快速找到满意解。
+    输入：
+    20 6
+    电脑 200 20
+    收音机 20 4
+    钟 175 10
+    花瓶 50 2
+    书 10 1
+    油画 90 9
+    """
+
+    def __init__(self, name, price, weight):
+        self.name = name
+        self.price = price
+        self.weight = weight
+
+    @property
+    def value(self):
+        """ 价格重量比 """
+        return self.price / self.weight
+
+
+def input_thing():
+    """ 输入物品信息 """
+    name_str, price_str, weight_str = input().split()
+    return name_str, int(price_str), int(weight_str)
+
+
+def main():
+    max_weight, num_of_things = map(int, input().split())
+    all_things = []
+    for _ in range(num_of_things):
+        all_things.append(Thing(*input_thing()))
+    all_things.sort(key=lambda x: x.value, reverse=True)
+    total_weight = 0
+    total_price = 0
+    for thing in all_things:
+        if total_weight + thing.weight <= max_weight:
+            print(f'小偷拿走了{thing.name}')
+            total_weight += thing.weight
+            total_price += thing.price
+    print(f'总价值:{total_price}美元')
+
+
+"""
+快速排序 - 选择枢轴对元素进行划分，左边都比枢轴小右边都比枢轴大
+"""
+
+
+def quick_sort(items, comp=lambda x, y: x <= y):
+    items = list(items)[:]
+    _quick_sort(items, 0, len(items) - 1, comp)
+    return items
+
+
+def _quick_sort(items, start, end, comp):
+    if start < end:
+        pos = _partition(items, start, end, comp)
+        _quick_sort(items, start, pos - 1, comp)
+        _quick_sort(items, pos + 1, end, comp)
+
+
+def _partition(items, start, end, comp):
+    pivot = items[end]
+    i = start - 1
+    for j in range(start, end):
+        if comp(items[j], pivot):
+            i += 1
+            items[i], items[j] = items[j], items[i]
+    items[i + 1], items[end] = items[end], items[i + 1]
+    return i + 1
+
+
+"""
+递归回溯法：叫称为试探法，按选优条件向前搜索，当搜索到某一步，
+发现原先选择并不优或达不到目标时，就退回一步重新选择，
+比较经典的问题包括骑士巡逻、八皇后和迷宫寻路等。
+"""
+SIZE = 5
+total = 0
+
+
+def print_board(board):
+    for row in board:
+        for col in row:
+            print(str(col).center(4), end='')
+        print()
+
+
+def patrol(board, row, col, step=1):
+    if row >= 0 and row < SIZE and col >= 0 and col < SIZE and board[row][col] == 0:
+        board[row][col] = step
+        if step == SIZE * SIZE:
+            global total
+            total += 1
+            print(f'第{total}种走法: ')
+            print_board(board)
+        patrol(board, row - 2, col - 1, step + 1)
+        patrol(board, row - 1, col - 2, step + 1)
+        patrol(board, row + 1, col - 2, step + 1)
+        patrol(board, row + 2, col - 1, step + 1)
+        patrol(board, row + 2, col + 1, step + 1)
+        patrol(board, row + 1, col + 2, step + 1)
+        patrol(board, row - 1, col + 2, step + 1)
+        patrol(board, row - 2, col + 1, step + 1)
+        board[row][col] = 0
+
+
+def case3():
+    board = [[0] * SIZE for _ in range(SIZE)]
+    patrol(board, SIZE - 1, SIZE - 1)
+
+
+"""  
+动态规划例子：子列表元素之和的最大值。
+
+说明：子列表指的是列表中索引（下标）连续的元素构成的列表；列表中的元素是int类型，可能包含正整数、0、负整数；程序输入列表中的元素，输出子列表元素求和的最大值，例如：
+
+输入：1 -2 3 5 -3 2
+
+输出：8
+
+输入：0 -2 3 5 -1 2
+
+输出：9
+
+输入：-9 -2 -3 -5 -3
+
+输出：-2
+"""
+
+def case4():
+    items = list(map(int, input().split()))
+    overall = partial = items[0]
+    for i in range(1, len(items)):
+        partial = max(items[i], partial + items[i])
+        overall = max(partial, overall)
+    print(overall)
+
+
+if __name__ == "__main__":
+    # case1()
+    # case2()
+    # main()
+    # case3()
+    case4()
